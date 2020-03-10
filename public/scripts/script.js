0c356264ac625ab95b2da037d7132415338e4fbe
IT27 <61205011+IT27@users.noreply.github.com>
2020-03-10 13:45:48 -0300

    Viagens
    Parte das Viagens

---
+++ public/scripts/script.js
@@ -0,0 +1,25 @@
+//Capturar os elementos pelo id
+var anterior = document.getElementById("anterior");
+var proximo = document.getElementById("proximo");
+var imagem = document.getElementById("imagem");
+var qtdImagem = new Number(10);
+
+var cont = 0 ;
+
+imagem.src ="/images/Fotos Escola/imagem"+cont+".jpg";
+
+anterior.onclick = function () {
+    if(cont > 0){
+        imagem.src = "";
+        cont--;
+        imagem.src = "/images/Fotos Escola/imagem"+cont+".jpg";
+    }
+};
+
+proximo.onclick = function () {
+    if( cont < (qtdImagem - 1)  ){
+        imagem.src ="";
+        cont++;
+        imagem.src = "/images/Fotos Escola/imagem"+cont+".jpg";
+    }
+};
