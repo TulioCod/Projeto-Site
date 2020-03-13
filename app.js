57dfe490594ab8202772f8775df7247370c69109
soubetinho <etinho1997@hotmail.com>
2020-03-13 16:34:16 -0300

    modc it

--- app.js
+++ app.js
@@ -48,7 +48,9 @@ app.get("/eventos/campeonatos/campsala", function(req, res) {
 app.get("/eventos/campeonatos/jemg", function(req, res) {
     res.render('admin/jemg') 
 })
-
+app.get("/eventos/campeonatos/jemg/gouveia", function(req, res) {
+    res.render("admin/jemgouv")
+})
 app.get("/localizacao", function(req, res) {
         res.render("admin/localizacao")
     })
